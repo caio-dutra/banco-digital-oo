7637460a1dfd4837facbd90c55d6bf9831570145
Caio Dutra Santos <93456721+caio-dutra@users.noreply.github.com>
2022-06-13 17:46:38 -0300

    encapsulamento/encapsulation

--- src/Banco.java
+++ src/Banco.java
@@ -3,4 +3,13 @@ public class Banco {
 	
 	private String nome;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,15 @@
 
 public class Cliente {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 
 }
